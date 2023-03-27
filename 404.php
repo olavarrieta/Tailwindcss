eaa5f04880380307bae00081e78d965256ce1501
Alvin Olavarrieta <alvinolavarrieta@gmail.com>
2023-03-27 15:16:37 -0400

    Initial push

---
+++ 404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" class="sm:scroll-smooth">
+
+<head>
+   <meta charset="UTF-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>Acme Rockets</title>
+   <link rel="stylesheet" href="/assets/css/styles.css" />
+   <script src="/assets/js/main.js" defer></script>
+</head>
+
+<body class="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
+
+   <main class="mx-auto max-w-4xl">
+      <section id="hero" class="widescreen:section-min-height tallscreen:section-min-height mb-12 flex scroll-mt-40 flex-col-reverse items-center justify-center gap-8 p-6 sm:flex-row">
+         <article class="sm:w-1/2">
+            <h2 class="max-w-md text-center text-4xl font-bold text-slate-900 dark:text-white sm:text-left sm:text-5xl">
+               404
+               <span class="text-indigo-700 dark:text-indigo-300">The Page Was</span>
+               Not Found...
+            </h2>
+            <p class="mt-4 max-w-md text-center text-2xl text-slate-700 dark:text-slate-400 sm:text-left">
+               Go back to the <a href="/" class="text-indigo-700 underline hover:underline">home page</a> and try again...
+            </p>
+         </article>
+         <img class="w-1/2" src="/assets/images/rocketdab.png" alt="Rocket Dab" />
+      </section>
+   </main>
+
+</body>
+
+</html>
